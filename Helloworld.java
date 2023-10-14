ceeb02903c65cb5c612f2fe8cb8d50681f48ec65
priiyakmore <priiyakmore@gmail.com>
2023-10-14 22:42:25 +0530

    my mavenjava file

---
+++ Helloworld.java
@@ -0,0 +1,9 @@
+// Your First Program
+
+class Helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+		System.out.println("Hello, maha!");
+		System.out.println("Hello, dear studnts!");
+    }
+}
